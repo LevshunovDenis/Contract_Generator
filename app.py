4f0fd170327256574482f7b5dbc126b44c3d097a
levshunovdennis@gmail.com <levshunovdennis@gmail.com>
2025-07-04 18:55:49 +0300

    add comment

--- app.py
+++ app.py
@@ -15,9 +15,7 @@ def get_company_info(unp):
         response = requests.get(url)
         response.raise_for_status()  # Проверим на успешный ответ от сервера
         data = response.json()
-
-        # Проверяем, если в ответе есть нужные данные
-        row = data.get('row', {})
+        row = data.get('row', {}) # Проверяем, если в ответе есть нужные данные
         if row:
             return {
                 'vunp': row.get('vunp', 'Неизвестно'),
@@ -40,36 +38,28 @@ def get_company_info(unp):
 def number_to_words(amount):
     rubles = int(amount)
     kopecks = round((amount - rubles) * 100)
-
-    # Пропись рублей
-    rubles_words = num2words(rubles, lang='ru', to='cardinal')
+    rubles_words = num2words(rubles, lang='ru', to='cardinal') # Пропись рублей
     last_digit = rubles % 10
     last_two_digits = rubles % 100
-
-    if 11 <= last_two_digits <= 14:
-        ruble_word = "рублей"
-    elif last_digit == 1:
-        ruble_word = "рубль"
-    elif 2 <= last_digit <= 4:
-        ruble_word = "рубля"
-    else:
-        ruble_word = "рублей"
-
-    # Склонение копеек
-    last_digit_k = kopecks % 10
-    last_two_digits_k = kopecks % 100
-
-    if 11 <= last_two_digits_k <= 14:
-        kopeck_word = "копеек"
-    elif last_digit_k == 1:
-        kopeck_word = "копейка"
-    elif 2 <= last_digit_k <= 4:
-        kopeck_word = "копейки"
-    else:
-        kopeck_word = "копеек"
-
-    # Строка прописью
-    result_text = f"{rubles_words} {ruble_word}, {kopecks:02d} {kopeck_word}"
+    if 11 <= last_two_digits <= 14: # Склонение рублей
+        ruble_word = "рублей" # Склонение рублей
+    elif last_digit == 1: # Склонение рублей
+        ruble_word = "рубль" # Склонение рублей
+    elif 2 <= last_digit <= 4: # Склонение рублей
+        ruble_word = "рубля" # Склонение рублей
+    else: # Склонение рублей
+        ruble_word = "рублей" # Склонение рублей
+    last_digit_k = kopecks % 10 # Склонение копеек
+    last_two_digits_k = kopecks % 100 # Склонение копеек
+    if 11 <= last_two_digits_k <= 14: # Склонение копеек
+        kopeck_word = "копеек" # Склонение копеек
+    elif last_digit_k == 1: # Склонение копеек
+        kopeck_word = "копейка" # Склонение копеек
+    elif 2 <= last_digit_k <= 4: # Склонение копеек
+        kopeck_word = "копейки" # Склонение копеек
+    else: # Склонение копеек
+        kopeck_word = "копеек" # Склонение копеек
+    result_text = f"{rubles_words} {ruble_word}, {kopecks:02d} {kopeck_word}" # Строка прописью
 
 
 
@@ -79,22 +69,22 @@ def number_to_words(amount):
 
 
     # ОКонвертируем даты поставки в более читаемый вид
+
+# Функция для преобразования даты в нужный формат
 def convert_date_format(date_str):
-    # Преобразуем строку даты в объект datetime
     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-    # Преобразуем объект datetime обратно в строку в нужном формате
     return date_obj.strftime('%d.%m.%Y')
    
 
 
     # Обработка отсутствия бюджет + внебюджет
+
+# Обработка точек и запытях
 def safe_float(val):
     try:
         return float(val.replace(',', '.'))  # поддержка запятых в числе
     except (ValueError, AttributeError):
         return 0.0
-
-
 
 # Функция для получения сегодняшней даты
 def get_today_date():
@@ -102,41 +92,40 @@ def get_today_date():
     formatted_date = today.strftime("%d.%m.%Y")
     return formatted_date
 
-
-
 # Запуск файла index
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
+# Генерация и заполнение документа Word
 @app.route('/generate_word', methods=['POST'])
 def generate_word():
-    # Получаем данные из формы
-    finance_type = request.form.get('finance_type')  
-    nomer_dogovora = request.form.get('nomer_dogovora')
-    centr_podpis = request.form.get('centr_podpis')
-    postavchik_unp = request.form.get('postavchik')
-    v_interesah = request.form.get('v_interesah')
-    summа_dogovora = float(request.form.get('summa_dogovora'))  # Преобразуем сумму в float
-    summa_budget = safe_float(request.form.get('summa_budget', 0))
-    summa_offbudget = safe_float(request.form.get('summa_offbudget', 0))
-    summa_propis = number_to_words(summа_dogovora)  
-    summa_budget = number_to_words(summa_budget)
-    summa_offbudget = number_to_words(summa_offbudget)# Получаем сумму прописью
-    metod_oplata = request.form.get('metod_oplata')
-    first_date = convert_date_format(request.form.get('first_date'))
-    last_date = convert_date_format(request.form.get('last_date'))
-    metod_post = request.form.get('metod_post')
-    adress_post = request.form.get('adress_post')
-    rekviz_post = request.form.get('rekviz_post')
-    UNP = request.form.get('UNP')
-    email_post = request.form.get('email_post')
-    phone = request.form.get('phone')
-    bank = request.form.get('bank')
-    podpis = request.form.get('podpis')
+    finance_type = request.form.get('finance_type')  # Получаем данные из формы 
+    nomer_dogovora = request.form.get('nomer_dogovora') # Получаем данные из формы 
+    centr_podpis = request.form.get('centr_podpis') # Получаем данные из формы 
+    postavchik_unp = request.form.get('postavchik') # Получаем данные из формы 
+    v_interesah = request.form.get('v_interesah') # Получаем данные из формы 
+    summа_dogovora = float(request.form.get('summa_dogovora'))  # Получаем данные из формы и преобразуем сумму в float
+    summa_budget = safe_float(request.form.get('summa_budget', 0)) # Получаем данные из формы и преобразуем сумму в float
+    summa_offbudget = safe_float(request.form.get('summa_offbudget', 0)) # Получаем данные из формы и преобразуем сумму в float
+    summa_propis = number_to_words(summа_dogovora)  # Получаем данные из формы и получаем сумму прописью
+    summa_budget = number_to_words(summa_budget) # Получаем данные из формы и получаем сумму прописью
+    summa_offbudget = number_to_words(summa_offbudget) # Получаем данные из формы и получаем сумму прописью
+    metod_oplata = request.form.get('metod_oplata') # Получаем данные из формы 
+    first_date = convert_date_format(request.form.get('first_date')) # Конвертация даты в нужный формат
+    last_date = convert_date_format(request.form.get('last_date')) # Конвертация даты в нужный формат
+    metod_post = request.form.get('metod_post') # Получаем данные из формы 
+    adress_post = request.form.get('adress_post') # Получаем данные из формы 
+    rekviz_post = request.form.get('rekviz_post') # Получаем данные из формы 
+    UNP = request.form.get('UNP') # Получаем данные из формы 
+    email_post = request.form.get('email_post') # Получаем данные из формы 
+    phone = request.form.get('phone') # Получаем данные из формы 
+    bank = request.form.get('bank') # Получаем данные из формы 
+    podpis = request.form.get('podpis') # Получаем данные из формы 
     
+
+
+    # на основании переменной centr_podpis определяем данные для автозаполнения
     centr_podpisant = ''
     centr_podpisant_fio = ''
     if centr_podpis == "sindeev":
@@ -165,8 +154,7 @@ def generate_word():
         "Учреждение 'Кормянский территориальный центр социального обслуживания населения'": "2018",
         "Сектор спорта и туризма Кормянского районного исполнительного комитета": "1020"
     }
-    # Получаем УНК в зависимости от выбранного заказчика
-    UNK = UNK_dict.get(v_interesah, "Неизвестно")
+    UNK = UNK_dict.get(v_interesah, "Неизвестно") # Получаем УНК в зависимости от выбранного заказчика
 
 
 
@@ -185,8 +173,7 @@ def generate_word():
         "Учреждение 'Кормянский территориальный центр социального обслуживания населения'": "45 от 03.01.2025г",
         "Сектор спорта и туризма Кормянского районного исполнительного комитета": "43 от 16.10.2023г"
     }
-    # Получаем номер договора обслуживания организации в зависимости от выбранного заказчика 
-    dog_obs = dog_obs_dict.get(v_interesah, "Неизвестно")
+    dog_obs = dog_obs_dict.get(v_interesah, "Неизвестно") # Получаем номер договора обслуживания организации в зависимости от выбранного заказчика 
 
 
 
@@ -205,8 +192,7 @@ def generate_word():
         "Учреждение 'Кормянский территориальный центр социального обслуживания населения'": "директора Воргановой Натальи Ивановны, действующей на основании Устава, ",
         "Сектор спорта и туризма Кормянского районного исполнительного комитета": "заведующего сетором Гериловича Евгения Витальевича, действующего на основании Положения о секторе, "
     }
-    # Получаем подписанта и его основания от выбранного заказчика
-    podpis = podpis_dict.get(v_interesah, "Неизвестно")  # Если метки нет в словаре, то присваиваем "Неизвестно"
+    podpis = podpis_dict.get(v_interesah, "Неизвестно")  # Получаем подписанта и его основания от выбранного заказчика
 
 
 
@@ -225,8 +211,7 @@ def generate_word():
         "Учреждение 'Кормянский территориальный центр социального обслуживания населения'": "Н.И. Ворганова",
         "Сектор спорта и туризма Кормянского районного исполнительного комитета": "Е. В. Герилович"
     }
-    # Получаем ФИО подписанта в конце документа от выбранного заказчика
-    podpis_fio = podpis_fio_dict.get(v_interesah, "Неизвестно")
+    podpis_fio = podpis_fio_dict.get(v_interesah, "Неизвестно") # Получаем ФИО подписанта в конце документа от выбранного заказчика
     
 
 
@@ -245,9 +230,9 @@ def generate_word():
         "Учреждение 'Кормянский территориальный центр социального обслуживания населения'": "Учреждение «Кормянский территориальный центр социального обслуживания населения» адрес: 247173, Гомельская область, Кормянский район, г.п. Корма, ул. Абатурова, 44.р/с BY70AKBB36044007123083200000, БИК  AKBBBY2Х, ЦБУ № 314 ОАО «АСБ Беларусбанк», УНК 20180,  УНП 490321424 Тел.: 8 02337 4 21 23,  4 21 57",
         "Сектор спорта и туризма Кормянского районного исполнительного комитета": "Сектор спорта и туризма Кормянского районного исполнительного комитета, 247173, Гомельская область, Кормянский район, г.п. Корма, ул. Ильющенко, 34, р/с, BY10AKBB36044200001260000000, БИК  AKBBBY2Х, ЦБУ № 314 ОАО «АСБ Беларусбанк», УНК 1020,  УНП 401170731 Тел. +375 25 920 70 59"
     }
-    # Получаем реквизиты от выбранного заказчика
-    rekviz_value = rekviz_dict.get(v_interesah, "Неизвестно")
+    rekviz_value = rekviz_dict.get(v_interesah, "Неизвестно") # Получаем реквизиты от выбранного заказчика
     
+    # В зависимости от типа финансирования правильно прописываем пункт 3.1
     if finance_type == 'budget':
         summa_propis = f"Общая сумма Договора составляет {summa_propis} белорусских рублей."
     elif finance_type == 'offbudget':
@@ -257,6 +242,7 @@ def generate_word():
     else:
         summa_propis = "Тип финансирования не определён."
 
+    # В зависимости от типа финансирования правильно прописываем пункт 3.2
     if finance_type == 'budget' and v_interesah == "Отдел образования Кормянского районного исполнительного комитета":
         istochnik = f"Источник финансирования - районный бюджет, УНК - 20100"
     elif finance_type == 'offbudget' and v_interesah == "Отдел образования Кормянского районного исполнительного комитета":
@@ -343,30 +329,21 @@ def generate_word():
     else:
         metod_oplata = "" 
         full_postavka = ""
-
-        
-       # Получаем полное наименование поставщика через УНП
+  
+    # Получаем полное наименование поставщика через УНП
     company_info = get_company_info(postavchik_unp)
-
-
 
     # Получаем сегодняшнюю дату
     today_date = get_today_date()
 
-
-
-    # Путь к шаблону Word документа
+    # Путь к шаблону Word документа в зависимости от типа финансирования
     if finance_type == 'offbudget':
         template_path = os.path.join('templates', 'template_offbudget.docx')
     else:
         template_path = os.path.join('templates', 'template.docx')
 
-
-
     # Загружаем шаблон
     doc = DocxTemplate(template_path)
-
-
 
     # Место для замены меток на значения из формы
     context = {
@@ -380,7 +357,6 @@ def generate_word():
         'istochnik': istochnik,
         'metod_oplata': metod_oplata,
         'full_postavka': full_postavka,
-
         'first_date': first_date,
         'last_date': last_date,
         'metod_post': metod_post,
@@ -398,25 +374,16 @@ def generate_word():
         'podpis_fio': podpis_fio,
     }
 
-
-
     # Заполняем шаблон
     doc.render(context)
-
-
 
     # Сохраняем файл на сервере
     output_path = os.path.join('static', f'generated_{nomer_dogovora}.docx')
     doc.save(output_path)
 
-
-
     # Отправляем файл пользователю
     return send_file(output_path, as_attachment=True)
    
-
-
-    # Запускаем программу
+# Запускаем программу
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
-
